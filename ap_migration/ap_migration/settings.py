658edb2f01ebf953c2b037184c0b699de74d91cc
Attendance Project <attendanceproject@Shao-Chengs-MacBook-Air.local>
2013-12-20 15:33:49 -0800

    added new model graph for the old database

--- ap_migration/ap_migration/settings.py
+++ ap_migration/ap_migration/settings.py
@@ -122,6 +122,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'migrate',
     'south',
+    'django_extensions',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
